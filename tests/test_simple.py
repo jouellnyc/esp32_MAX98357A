d4025e2c3606dc9b1614c37c3c9a08261d319db9
John Ouellette <jouellnyc@gmail.com>
2025-12-27 21:16:05 -0500

    print

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -4,7 +4,7 @@ sdcard_helper.mount()
 count = 0  # <--- Initialize ONCE here
 for x in range(0, 100):
     try:
-        os.listdir('/sd/')
+        print(os.listdir('/sd/'))
     except Exception as e:
         # This only runs if there WAS an error
         print(f"Error on attempt {x}: {e}")
