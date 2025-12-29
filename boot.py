4e28f9507d71f72d2d04907a74e33429aa274a9c
John Ouellette <jouellnyc@gmail.com>
2025-12-29 13:38:11 -0500

    Disable usb mounting

--- boot.py
+++ boot.py
@@ -3,3 +3,15 @@ import storage
 # This tells CircuitPython: 
 # "Give the MICROCONTROLLER write access, and make it Read-Only for the PC"
 storage.remount("/", False)
+
+import supervisor
+supervisor.runtime.autoreload = False
+
+
+import storage
+
+# This hides the CIRCUITPY drive from the host computer (Linux/Windows/Mac)
+storage.disable_usb_drive()
+
+print("USB Drive Disabled")
+
